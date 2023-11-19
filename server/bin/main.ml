0ea6ed126e1a3efeda285e627dd8877e03c01b5a
Jake Quinter <hello@jakequinter.io>
2023-11-19 18:18:58 -0500

    cleanup

--- server/bin/main.ml
+++ server/bin/main.ml
@@ -1,6 +1,15 @@
 open Routes.Users
 
+let init_pool () =
+  let uri = Uri.make ~scheme:"sqlite3" ~path:"../vow.db" () in
+  match Caqti_lwt.connect_pool ~max_size:10 uri with
+  | Ok pool -> pool
+  | Error err -> failwith (Caqti_error.show err)
+
+let pool = init_pool ()
+
 let () =
   Dream.run @@ Dream.logger
   @@ Dream.router
-       ([ Dream.get "/" (fun _ -> Dream.html "Hello, world!") ] @ users_routes)
+       ([ Dream.get "/" (fun _ -> Dream.html "Hello, world!") ]
+       @ users_routes pool)
--- server/lib/routes/users.ml
+++ server/lib/routes/users.ml
@@ -1,71 +1,67 @@
 open Lwt.Syntax
 
-let get_users connection =
-  let users = ref [] in
-  let query = "SELECT * FROM users;" in
-  let cb row _ = users := Array.to_list row :: !users in
-  match Sqlite3.exec_not_null ~cb connection query with
-  | Sqlite3.Rc.OK -> !users
-  | rc -> failwith (Sqlite3.Rc.to_string rc)
+(* GET: /users *)
+let get_users_request pool _request =
+  let get_users =
+    [%rapper
+      get_many
+        {sql| SELECT @int{id}, @string{bride_first_name}, @string{bride_last_name}, @string{groom_first_name}, @string{groom_last_name} FROM users |sql}]
+  in
+  let* users_result = Caqti_lwt.Pool.use (get_users ()) pool in
+  match users_result with
+  | Error e -> failwith (Caqti_error.show e)
+  | Ok users ->
+      let json =
+        `List
+          (List.map
+             (fun ( id,
+                    bride_first_name,
+                    bride_last_name,
+                    groom_first_name,
+                    groom_last_name ) ->
+               `Assoc
+                 [
+                   ("id", `Int id);
+                   ("bride_first_name", `String bride_first_name);
+                   ("bride_last_name", `String bride_last_name);
+                   ("groom_first_name", `String groom_first_name);
+                   ("groom_last_name", `String groom_last_name);
+                 ])
+             users)
+      in
+      Dream.json (Yojson.Safe.to_string json)
 
-let users_to_json users =
-  `List
-    (List.map
-       (fun user_fields ->
-         `Assoc
-           [
-             ("id", `Int (int_of_string (List.nth user_fields 0)));
-             ("name", `String (List.nth user_fields 1));
-           ])
-       users)
-
-let get_users_request _request =
-  let db = Sqlite3.db_open "../vow.db" in
-  let users = get_users db in
-  let json = users_to_json users in
-  Dream.json (Yojson.Safe.to_string json)
-
-let post_users_request request =
+(* POST: /users *)
+let post_users_request pool request =
   let* body = Dream.body request in
+  let data = Yojson.Safe.from_string body in
+  let open Yojson.Safe.Util in
+  let bride_first_name = data |> member "bride_first_name" |> to_string in
+  let bride_last_name = data |> member "bride_last_name" |> to_string in
+  let groom_first_name = data |> member "groom_first_name" |> to_string in
+  let groom_last_name = data |> member "groom_last_name" |> to_string in
 
-  (* Convert body into a JSON object *)
-  let json = Yojson.Safe.from_string body in
-
-  (* Extract values safely using Option monad *)
-  let safe_extract key =
-    match Yojson.Safe.Util.(json |> member key |> to_string_option) with
-    | Some value -> value
-    | None ->
-        let err_message = Printf.sprintf "Key %s not found in JSON" key in
-        Dream.log "%s\n" err_message;
-        failwith err_message (* This will terminate the function *)
+  (* Define the function to insert a user *)
+  let insert_user =
+    [%rapper
+      execute
+        {sql|
+           INSERT INTO users (bride_first_name, bride_last_name, groom_first_name, groom_last_name)
+           VALUES (%string{bride_first_name}, %string{bride_last_name}, %string{groom_first_name}, %string{groom_last_name})
+        |sql}]
   in
 
-  (* Use safe_extract function to extract the needed values *)
-  let bride_first_name = safe_extract "bride_first_name" in
-  let bride_last_name = safe_extract "bride_last_name" in
-  let groom_first_name = safe_extract "groom_first_name" in
-  let groom_last_name = safe_extract "groom_last_name" in
-
-  let db = Sqlite3.db_open "../vow.db" in
-  let insert_query =
-    Printf.sprintf
-      "INSERT INTO users (bride_first_name, bride_last_name, groom_first_name, \
-       groom_last_name) VALUES ('%s', '%s', '%s', '%s')"
-      bride_first_name bride_last_name groom_first_name groom_last_name
+  let* result =
+    Caqti_lwt.Pool.use
+      (fun db ->
+        insert_user ~bride_first_name ~bride_last_name ~groom_first_name
+          ~groom_last_name db)
+      pool
   in
 
-  match Sqlite3.exec db insert_query with
-  | Sqlite3.Rc.OK ->
-      Dream.log "Insert successful\n";
-      Dream.json ~status:`OK
-        (Yojson.Safe.to_string
-           (`Assoc [ ("message", `String "Insert successful") ]))
-  | rc ->
-      let error_msg = Sqlite3.Rc.to_string rc in
-      Dream.log "Insert failed with error %s\n" error_msg;
-      Dream.json ~status:`Internal_Server_Error
-        (Yojson.Safe.to_string (`Assoc [ ("message", `String error_msg) ]))
+  match result with
+  | Ok () -> Dream.json ~status:`Created ""
+  | Error e -> Dream.json ~status:`Internal_Server_Error (Caqti_error.show e)
 
 let cors_middleware inner_handler req =
   let new_headers =
@@ -84,10 +80,10 @@ let cors_middleware inner_handler req =
 
   response
 
-let users_routes =
+let users_routes pool =
   [
     Dream.options "/users" (fun req ->
         cors_middleware (fun _ -> Dream.empty `OK) req);
-    Dream.get "/users" get_users_request;
-    Dream.post "/users" post_users_request;
+    Dream.get "/users" (get_users_request pool);
+    Dream.post "/users" (post_users_request pool);
   ]
